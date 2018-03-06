00c9328e5e19a9dc4766ae574dc7e6afa7362809
cScot <cscot@nmd.so>
2018-03-05 23:39:52 -0800

    Updated creative tab Item Icon

--- src/main/java/cscot/basicnetherores/tabs/BasicNetherOresTab.java
+++ src/main/java/cscot/basicnetherores/tabs/BasicNetherOresTab.java
@@ -1,5 +1,7 @@
 package cscot.basicnetherores.tabs;
 
+import cscot.basicnetherores.init.BlockOreInit;
+import cscot.basicnetherores.init.ItemInit;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -8,6 +10,6 @@ public class BasicNetherOresTab extends CreativeTabs {
 	
 	public BasicNetherOresTab(String label) { super("basicnetherorestab");
 	this.setBackgroundImageName("basicnetheroresgui.png");}
-	public ItemStack getTabIconItem() { return new ItemStack(Items.QUARTZ);}
+	public ItemStack getTabIconItem() { return new ItemStack(BlockOreInit.ORE_NETHEREMERALD, 1);}
 		
 }
