f3aad798f4ac31688674819f3490ddb1452a7a98
cScot <cscot@nmd.so>
2018-03-06 17:35:35 -0800

    Updated Version for Release & Added Mod Image

--- src/main/java/cscot/basicnetherores/BasicNetherOres.java
+++ src/main/java/cscot/basicnetherores/BasicNetherOres.java
@@ -17,7 +17,7 @@ public class BasicNetherOres {
 
 	public static final String MOD_ID = "bno";
 	public static final String MOD_NAME = "Basic Nether Ores";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.12.2-1.0.0.0";
 	
 	public static final String CLIENTPROXY = "cscot.basicnetherores.proxy.ClientProxy";
 	public static final String COMMONPROXY = "cscot.basicnetherores.proxy.CommonProxy";
