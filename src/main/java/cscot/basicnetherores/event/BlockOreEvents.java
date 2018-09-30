a74a4800733b593deddc951de59c81422e1afd01
cScot <cscot@nmd.so>
2018-09-30 15:55:30 -0500

    added priority to the event

--- src/main/java/cscot/basicnetherores/event/BlockOreEvents.java
+++ src/main/java/cscot/basicnetherores/event/BlockOreEvents.java
@@ -10,6 +10,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 
@@ -17,8 +18,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public final class BlockOreEvents {
 
 
-    @SubscribeEvent
-    public static void onBlockBreak(BlockEvent.HarvestDropsEvent event) {
+    @SubscribeEvent(priority=EventPriority.HIGHEST, receiveCanceled=false)
+    public static void onBlockHarvest(BlockEvent.HarvestDropsEvent event) {
 
         if(!event.isSilkTouching()) {
 
