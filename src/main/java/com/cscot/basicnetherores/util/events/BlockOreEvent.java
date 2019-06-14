8c0af8c7e01883cc2a01e75f9b30099e8ae07c29
cScot <cscot@nmd.so>
2019-06-14 01:54:36 -0500

    Replaced onBlockHarvest with BreakEvent for PigmanGuard

--- src/main/java/com/cscot/basicnetherores/util/events/BlockOreEvent.java
+++ src/main/java/com/cscot/basicnetherores/util/events/BlockOreEvent.java
@@ -4,6 +4,9 @@ import com.cscot.basicnetherores.config.OreProtectionConfig;
 import com.cscot.basicnetherores.objects.BlockOreBase;
 import com.cscot.basicnetherores.util.helpers.BlockListHelper;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,12 +16,16 @@ import net.minecraftforge.fml.common.Mod;
 public class BlockOreEvent
 {
 
-    //When a Nether Ore is harvested it is checked against the following conditions
-    @SubscribeEvent (priority = EventPriority.HIGH, receiveCanceled = true)
-    public static void onBlockHarvested(BlockEvent.HarvestDropsEvent event) {
+    /** Reference to the Player who broke the block. If no player is available, use a EntityFakePlayer */
 
-        //Check to see if tool has Silk Touch
-        if(!event.isSilkTouching() && OreProtectionConfig.silkEffect.get()) {
+    @SubscribeEvent (priority = EventPriority.HIGH, receiveCanceled = true)
+    public static void BreakEvent(BlockEvent.BreakEvent event)
+    {
+
+        PlayerEntity player = event.getPlayer();
+        int isSilkTouching = EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, player.getHeldItemMainhand());
+
+        if(isSilkTouching < 1 && OreProtectionConfig.silkEffect.get()) {
 
             //Checks if the Config file is set to true
             if (OreProtectionConfig.pigmanGuard.get()) {
@@ -28,7 +35,7 @@ public class BlockOreEvent
                 //Compares the broken Block to a list generated in the Config File
                 if(BlockListHelper.protectedOres(block))
                 {
-                    BlockOreBase.pigmenGuards(event.getWorld().getWorld(), event.getPos(), event.getHarvester());
+                    BlockOreBase.pigmenGuards(event.getWorld().getWorld(), event.getPos(), player);
                 }
             }
         }
