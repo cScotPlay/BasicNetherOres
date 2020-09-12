464a61f3d47037f9291cccbbe439e8242bf8dcfa
cScotPlay <cscotplay@gmail.com>
2020-09-12 13:15:57 -0500

    fully integrated new ore features and generation

--- src/main/java/com/cscot/basicnetherores/world/OreGenerator.java
+++ src/main/java/com/cscot/basicnetherores/world/OreGenerator.java
@@ -1,6 +1,7 @@
 package com.cscot.basicnetherores.world;
 
 
+import com.cscot.basicnetherores.config.OreGenerationConfig;
 import com.cscot.basicnetherores.world.gen.feature.ModOreFeatures;
 import com.google.common.collect.Lists;
 import net.minecraft.util.RegistryKey;
@@ -23,96 +24,63 @@ public class OreGenerator
     {
         for (Map.Entry<RegistryKey<Biome>, Biome> biome : WorldGenRegistries.field_243657_i.getEntries()) {
 
-            if (biome.getValue().getCategory().equals(Biome.Category.NETHER)){
+            if (biome.getValue().getCategory().equals(Biome.Category.NETHER))
+            {
+                if(OreGenerationConfig.emeraldGeneration.get()){
                 addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_EMERALD_NETHER);
-
-
-
-
-
-                /*if (OreGenerationConfig.emeraldGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netheremerald_ore.getDefaultState(), OreGenerationConfig.emeraldVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.emeraldPerChunk.get(), OreGenerationConfig.emeraldMinHeight.get(), 0, OreGenerationConfig.emeraldMaxHeight.get()))));
                 }
 
-                if (OreGenerationConfig.diamondGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netherdiamond_ore.getDefaultState(), OreGenerationConfig.diamondVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.diamondPerChunk.get(), OreGenerationConfig.diamondMinHeight.get(), 0, OreGenerationConfig.diamondMaxHeight.get()))));
+                if(OreGenerationConfig.diamondGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_DIAMOND_NETHER);
                 }
 
-                if (OreGenerationConfig.redstoneGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netherredstone_ore.getDefaultState(), OreGenerationConfig.redstoneVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.redstonePerChunk.get(), OreGenerationConfig.redstoneMinHeight.get(), 0, OreGenerationConfig.redstoneMaxHeight.get()))));
+                if(OreGenerationConfig.redstoneGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_REDSTONE_NETHER);
                 }
 
-                if (OreGenerationConfig.lapisGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netherlapis_ore.getDefaultState(), OreGenerationConfig.lapisVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.lapisPerChunk.get(), OreGenerationConfig.lapisMinHeight.get(), 0, OreGenerationConfig.lapisMaxHeight.get()))));
+                if(OreGenerationConfig.lapisGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_LAPIS_NETHER);
                 }
 
-                if (OreGenerationConfig.coalGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.nethercoal_ore.getDefaultState(), OreGenerationConfig.coalVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.coalPerChunk.get(), OreGenerationConfig.coalMinHeight.get(), 0, OreGenerationConfig.coalMaxHeight.get()))));
+                if(OreGenerationConfig.coalGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_COAL_NETHER);
                 }
 
-                if (OreGenerationConfig.silverGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.nethersilver_ore.getDefaultState(), OreGenerationConfig.silverVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.silverPerChunk.get(), OreGenerationConfig.silverMinHeight.get(), 0, OreGenerationConfig.silverMaxHeight.get()))));
+                if(OreGenerationConfig.silverGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_SILVER_NETHER);
                 }
 
-                if (OreGenerationConfig.ironGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netheriron_ore.getDefaultState(), OreGenerationConfig.ironVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.ironPerChunk.get(), OreGenerationConfig.ironMinHeight.get(), 0, OreGenerationConfig.ironMaxHeight.get()))));
+                if(OreGenerationConfig.ironGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_IRON_NETHER);
                 }
 
-                if (OreGenerationConfig.leadGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netherlead_ore.getDefaultState(), OreGenerationConfig.leadVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.leadPerChunk.get(), OreGenerationConfig.leadMinHeight.get(), 0, OreGenerationConfig.leadMaxHeight.get()))));
+                if(OreGenerationConfig.leadGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_LEAD_NETHER);
                 }
 
-                if (OreGenerationConfig.nickelGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.nethernickel_ore.getDefaultState(), OreGenerationConfig.nickelVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.nickelPerChunk.get(), OreGenerationConfig.nickelMinHeight.get(), 0, OreGenerationConfig.nickelMaxHeight.get()))));
+                if(OreGenerationConfig.nickelGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_NICKEL_NETHER);
                 }
 
-                if (OreGenerationConfig.copperGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.nethercopper_ore.getDefaultState(), OreGenerationConfig.copperVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.copperPerChunk.get(), OreGenerationConfig.copperMinHeight.get(), 0, OreGenerationConfig.copperMaxHeight.get()))));
+                if(OreGenerationConfig.copperGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_COPPER_NETHER);
                 }
 
-                if (OreGenerationConfig.aluminumGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netheraluminum_ore.getDefaultState(), OreGenerationConfig.aluminumVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.aluminumPerChunk.get(), OreGenerationConfig.aluminumMinHeight.get(), 0, OreGenerationConfig.aluminumMaxHeight.get()))));
+                if(OreGenerationConfig.aluminumGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_ALUMINUM_NETHER);
                 }
 
-                if (OreGenerationConfig.tinGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.nethertin_ore.getDefaultState(), OreGenerationConfig.tinVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.tinPerChunk.get(), OreGenerationConfig.tinMinHeight.get(), 0, OreGenerationConfig.tinMaxHeight.get()))));
+                if(OreGenerationConfig.tinGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_TIN_NETHER);
                 }
 
-                if (OreGenerationConfig.osmiumGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netherosmium_ore.getDefaultState(), OreGenerationConfig.osmiumVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.osmiumPerChunk.get(), OreGenerationConfig.osmiumMinHeight.get(), 0, OreGenerationConfig.osmiumMaxHeight.get()))));
+                if(OreGenerationConfig.osmiumGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_OSMIUM_NETHER);
                 }
 
-                if (OreGenerationConfig.uraniumGeneration.get()) {
-                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                            new OreFeatureConfig(NETHERRACK, BlockOreList.netheruranium_ore.getDefaultState(), OreGenerationConfig.uraniumVeinSize.get())).withPlacement(Placement.COUNT_RANGE.configure(
-                            new CountRangeConfig(OreGenerationConfig.uraniumPerChunk.get(), OreGenerationConfig.uraniumMinHeight.get(), 0, OreGenerationConfig.uraniumMaxHeight.get()))));
-                }*/
+                if(OreGenerationConfig.uraniumGeneration.get()){
+                    addNetherOres(biome.getValue(), GenerationStage.Decoration.UNDERGROUND_ORES, ModOreFeatures.ORE_URANIUM_NETHER);
+                }
             }
         }
     }
@@ -120,13 +88,16 @@ public class OreGenerator
     //Add Nether Ores Method adapted from DrunkBlood's Lucky Ore mod https://github.com/DrunkBlood/Lucky-Ore
     public static void addNetherOres(Biome biome, GenerationStage.Decoration decoration, ConfiguredFeature<?, ?> configuredFeature)
     {
-        List<List<Supplier<ConfiguredFeature<?, ?>>>> biomeFeatures = new ArrayList<>(
-                biome.func_242440_e().func_242498_c());
-        while (biomeFeatures.size() <= decoration.ordinal()) {
+        List<List<Supplier<ConfiguredFeature<?, ?>>>> biomeFeatures = new ArrayList<>(biome.func_242440_e().func_242498_c());
+
+        while (biomeFeatures.size() <= decoration.ordinal())
+        {
             biomeFeatures.add(Lists.newArrayList());
         }
         List<Supplier<ConfiguredFeature<?, ?>>> features = new ArrayList<>(biomeFeatures.get(decoration.ordinal()));
+
         features.add(() -> configuredFeature);
+
         biomeFeatures.set(decoration.ordinal(), features);
 
         ObfuscationReflectionHelper.setPrivateValue(BiomeGenerationSettings.class, biome.func_242440_e(), biomeFeatures,
--- src/main/java/com/cscot/basicnetherores/world/gen/feature/ModOreFeatures.java
+++ src/main/java/com/cscot/basicnetherores/world/gen/feature/ModOreFeatures.java
@@ -27,10 +27,166 @@ public class ModOreFeatures
                             OreGenerationConfig.emeraldMaxHeight.get())))
             .func_242728_a().func_242731_b(OreGenerationConfig.emeraldPerChunk.get());
 
+    public static ConfiguredFeature<?, ?> ORE_DIAMOND_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netherdiamond_ore.getDefaultState(),
+                    OreGenerationConfig.diamondVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.diamondMinHeight.get(),
+                            0,
+                            OreGenerationConfig.diamondMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.diamondPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_REDSTONE_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netherredstone_ore.getDefaultState(),
+                    OreGenerationConfig.redstoneVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.redstoneMinHeight.get(),
+                            0,
+                            OreGenerationConfig.redstoneMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.redstonePerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_LAPIS_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netherlapis_ore.getDefaultState(),
+                    OreGenerationConfig.lapisVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.lapisMinHeight.get(),
+                            0,
+                            OreGenerationConfig.lapisMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.lapisPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_COAL_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.nethercoal_ore.getDefaultState(),
+                    OreGenerationConfig.coalVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.coalMinHeight.get(),
+                            0,
+                            OreGenerationConfig.coalMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.coalPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_SILVER_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.nethersilver_ore.getDefaultState(),
+                    OreGenerationConfig.silverVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.silverMinHeight.get(),
+                            0,
+                            OreGenerationConfig.silverMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.silverPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_IRON_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netheriron_ore.getDefaultState(),
+                    OreGenerationConfig.ironVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.ironMinHeight.get(),
+                            0,
+                            OreGenerationConfig.ironMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.ironPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_LEAD_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netherlead_ore.getDefaultState(),
+                    OreGenerationConfig.leadVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.leadMinHeight.get(),
+                            0,
+                            OreGenerationConfig.leadMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.leadPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_NICKEL_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.nethernickel_ore.getDefaultState(),
+                    OreGenerationConfig.nickelVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.nickelMinHeight.get(),
+                            0,
+                            OreGenerationConfig.nickelMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.nickelPerChunk.get());
+    
+    public static ConfiguredFeature<?, ?> ORE_COPPER_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.nethercopper_ore.getDefaultState(),
+                    OreGenerationConfig.copperVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.copperMinHeight.get(),
+                            0,
+                            OreGenerationConfig.copperMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.copperPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_ALUMINUM_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netheraluminum_ore.getDefaultState(),
+                    OreGenerationConfig.aluminumVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.aluminumMinHeight.get(),
+                            0,
+                            OreGenerationConfig.aluminumMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.aluminumPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_TIN_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.nethertin_ore.getDefaultState(),
+                    OreGenerationConfig.tinVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.tinMinHeight.get(),
+                            0,
+                            OreGenerationConfig.tinMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.tinPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_OSMIUM_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netherosmium_ore.getDefaultState(),
+                    OreGenerationConfig.osmiumVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.osmiumMinHeight.get(),
+                            0,
+                            OreGenerationConfig.osmiumMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.osmiumPerChunk.get());
+
+    public static ConfiguredFeature<?, ?> ORE_URANIUM_NETHER = Feature.ORE
+            .withConfiguration(new OreFeatureConfig(
+                    OreFeatureConfig.FillerBlockType.field_241883_b, BlockOreList.netheruranium_ore.getDefaultState(),
+                    OreGenerationConfig.uraniumVeinSize.get()))
+            .withPlacement(Placement.field_242907_l.configure(
+                    new TopSolidRangeConfig(
+                            OreGenerationConfig.uraniumMinHeight.get(),
+                            0,
+                            OreGenerationConfig.uraniumMaxHeight.get())))
+            .func_242728_a().func_242731_b(OreGenerationConfig.uraniumPerChunk.get());
+
 
     public static void initModFeatures ()
     {
         Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_emerald_nether"), ORE_EMERALD_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_diamond_nether"), ORE_DIAMOND_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_redstone_nether"), ORE_REDSTONE_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_lapis_nether"), ORE_LAPIS_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_coal_nether"), ORE_COAL_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_silver_nether"), ORE_SILVER_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_iron_nether"), ORE_IRON_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_lead_nether"), ORE_LEAD_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_nickel_nether"), ORE_NICKEL_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_copper_nether"), ORE_COPPER_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_aluminum_nether"), ORE_ALUMINUM_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_tin_nether"), ORE_TIN_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_osmium_nether"), ORE_OSMIUM_NETHER);
+        Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(modID, "ore_uranium_nether"), ORE_URANIUM_NETHER);
     }
 
 }
