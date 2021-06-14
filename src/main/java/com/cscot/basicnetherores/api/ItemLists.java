4e025707cf9972d54a270136076805d01f475693
cScot <cscot@nmd.so>
2021-06-13 19:32:02 -0500

    removed copper items and added raw ore items

--- src/main/java/com/cscot/basicnetherores/api/ItemLists.java
+++ src/main/java/com/cscot/basicnetherores/api/ItemLists.java
@@ -16,7 +16,7 @@ public class ItemLists
 
     //Ingots List
     public static Item ALUMINUM_INGOT = addItem("aluminum_ingot", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
-    public static Item COPPER_INGOT = addItem("copper_ingot", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    //public static Item COPPER_INGOT = addItem("copper_ingot", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item LEAD_INGOT = addItem("lead_ingot", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item NICKEL_INGOT = addItem("nickel_ingot", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item SILVER_INGOT = addItem("silver_ingot", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
@@ -26,12 +26,21 @@ public class ItemLists
 
     //Nugget List
     public static Item ALUMINUM_NUGGET = addItem("aluminum_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
-    public static Item COPPER_NUGGET = addItem("copper_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    //public static Item COPPER_NUGGET = addItem("copper_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item LEAD_NUGGET = addItem("lead_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item NICKEL_NUGGET = addItem("nickel_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item SILVER_NUGGET = addItem("silver_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item OSMIUM_NUGGET = addItem("osmium_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
     public static Item TIN_NUGGET = addItem("tin_nugget", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+
+    //Raw Ore List
+    public static Item RAW_ALUMINUM = addItem("raw_aluminum", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    public static Item RAW_LEAD = addItem("raw_lead", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    public static Item RAW_NICKEL = addItem("raw_nickel", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    public static Item RAW_SILVER = addItem("raw_silver", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    public static Item RAW_TIN = addItem("raw_tin", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    public static Item RAW_OSMIUM = addItem("raw_osmium", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
+    public static Item RAW_URANIUM = addItem("raw_uranium", new Item((new Item.Settings()).group(BasicNetherOres.ITEMGROUP)));
 
 
     public static <I extends BlockItem> I add(String name, I item) {
