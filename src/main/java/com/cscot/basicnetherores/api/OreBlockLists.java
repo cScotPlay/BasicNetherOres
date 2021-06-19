69d9022057ce77e863ebb8f114adaa100b7c920b
cScot <cscot@nmd.so>
2021-06-19 09:01:18 -0500

    added modded nether redstone block

--- src/main/java/com/cscot/basicnetherores/api/OreBlockLists.java
+++ src/main/java/com/cscot/basicnetherores/api/OreBlockLists.java
@@ -2,8 +2,10 @@ package com.cscot.basicnetherores.api;
 
 import com.cscot.basicnetherores.BasicNetherOres;
 import com.cscot.basicnetherores.blocks.ModOreBlock;
-import net.minecraft.block.Block;
+import com.cscot.basicnetherores.blocks.ModRedstoneOreBlock;
+import net.minecraft.block.*;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,11 +16,11 @@ public class OreBlockLists
     public static final Map<Identifier, Block> OREBLOCKS = new HashMap<>();
 
     //This is all of the Ore Blocks that will be defined
-    public static Block NETHEREMERALD_ORE = add("netheremerald_ore", new ModOreBlock("netheremerald_ore"));
-    public static Block NETHERDIAMOND_ORE = add("netherdiamond_ore", new ModOreBlock("netherdiamond_ore"));
-    public static Block NETHERREDSTONE_ORE = add("netherredstone_ore", new ModOreBlock("netherredstone_ore"));
-    public static Block NETHERLAPIS_ORE = add("netherlapis_ore", new ModOreBlock("netherlapis_ore"));
-    public static Block NETHERCOAL_ORE = add("nethercoal_ore", new ModOreBlock("nethercoal_ore"));
+    public static Block NETHEREMERALD_ORE = add("netheremerald_ore", new ModOreBlock("netheremerald_ore", UniformIntProvider.create(3, 7)));
+    public static Block NETHERDIAMOND_ORE = add("netherdiamond_ore", new ModOreBlock("netherdiamond_ore", UniformIntProvider.create(3, 7)));
+    public static Block NETHERREDSTONE_ORE = add("netherredstone_ore", new ModRedstoneOreBlock("netherredstone_ore"));
+    public static Block NETHERLAPIS_ORE = add("netherlapis_ore", new ModOreBlock("netherlapis_ore", UniformIntProvider.create(2, 5)));
+    public static Block NETHERCOAL_ORE = add("nethercoal_ore", new ModOreBlock("nethercoal_ore", UniformIntProvider.create(0, 2)));
     public static Block NETHERSILVER_ORE = add("nethersilver_ore", new ModOreBlock("nethersilver_ore"));
     public static Block NETHERIRON_ORE = add("netheriron_ore", new ModOreBlock("netheriron_ore"));
     public static Block NETHERLEAD_ORE = add("netherlead_ore", new ModOreBlock("netherlead_ore"));
@@ -28,7 +30,6 @@ public class OreBlockLists
     public static Block NETHERTIN_ORE = add("nethertin_ore", new ModOreBlock("nethertin_ore"));
     public static Block NETHEROSMIUM_ORE = add("netherosmium_ore", new ModOreBlock("netherosmium_ore"));
     public static Block NETHERURANIUM_ORE = add("netheruranium_ore", new ModOreBlock("netheruranium_ore"));
-
 
     private static <oreBlock extends Block> oreBlock add(String name, oreBlock block) {
         OREBLOCKS.put(new Identifier(BasicNetherOres.MOD_ID, name), block);
---
+++ src/main/java/com/cscot/basicnetherores/blocks/ModRedstoneOreBlock.java
@@ -0,0 +1,82 @@
+package com.cscot.basicnetherores.blocks;
+
+import com.cscot.basicnetherores.BasicNetherOres;
+import com.cscot.basicnetherores.api.ItemLists;
+import com.cscot.basicnetherores.api.OreBlockLists;
+import com.cscot.basicnetherores.config.ModConfig;
+import com.cscot.basicnetherores.util.helpers.OreTooltipHelper;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.block.RedstoneOreBlock;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.mob.PiglinBrain;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.Properties;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+
+import java.util.List;
+import java.util.function.ToIntFunction;
+
+public class ModRedstoneOreBlock extends RedstoneOreBlock
+{
+
+    public ModRedstoneOreBlock(String oreName) {
+
+        super(FabricBlockSettings.of(Material.STONE)
+                .breakByTool(FabricToolTags.PICKAXES, 2)
+                .requiresTool()
+                .ticksRandomly()
+                .luminance(createLightLevelFromLitBlockState())
+                .strength(3.0f, 3.0f)
+                .sounds(BlockSoundGroup.NETHER_GOLD_ORE));
+
+        this.setDefaultState(this.getDefaultState().with(LIT, false));
+        new Identifier(BasicNetherOres.MOD_ID, oreName);
+
+        ItemLists.add(oreName, new BlockItem(this, new Item.Settings().group(BasicNetherOres.ITEMGROUP)));
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void appendTooltip(ItemStack stack, BlockView worldIn, List<Text> tooltip, TooltipContext flagIn)
+    {
+        if (this == OreBlockLists.NETHERREDSTONE_ORE){
+            if (!ModConfig.redstoneGeneration){
+                tooltip.add(new TranslatableText("tooltip.config.tip"));}
+            else tooltip.add(new TranslatableText(OreTooltipHelper.RedstoneOreTip.oreTip, ModConfig.redstoneMinHeight, ModConfig.redstoneMaxHeight));}
+    }
+
+    @Override
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        world.syncWorldEvent(player, 2001, pos, getRawIdFromState(state));
+        int isSilkTouching = EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, player.getMainHandStack());
+
+        if (state.isIn(BlockTags.GUARDED_BY_PIGLINS) && ModConfig.piglinGuard) {
+            if (ModConfig.silkEffect) {
+                if (isSilkTouching < 1) {
+                    PiglinBrain.onGuardedBlockInteracted(player, false);
+                }
+            }else PiglinBrain.onGuardedBlockInteracted(player, false);
+        }
+    }
+
+    private static ToIntFunction<BlockState> createLightLevelFromLitBlockState() {
+        return (state) -> (Boolean)state.get(Properties.LIT) ? 9 : 0;
+    }
+}
