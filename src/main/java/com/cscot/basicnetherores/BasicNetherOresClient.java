0d3c8b3dd63c28790d57937f733ca72dec1c0ff2
cScotPlay <cscotplay@gmail.com>
2022-03-06 11:37:21 -0800

    started compat for create

--- src/main/java/com/cscot/basicnetherores/BasicNetherOresClient.java
+++ src/main/java/com/cscot/basicnetherores/BasicNetherOresClient.java
@@ -3,6 +3,7 @@ package com.cscot.basicnetherores;
 import com.cscot.basicnetherores.client.ClientHandler;
 import com.cscot.basicnetherores.client.ShippedResourcePack;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class BasicNetherOresClient
@@ -15,6 +16,10 @@ public class BasicNetherOresClient
     public static void clientInit(final FMLClientSetupEvent event)
     {
         ShippedResourcePack.extractFiles("Basic Nether Ores 16x");
+//        if(ModList.get().isLoaded("create"))
+//        {
+//            BasicNetherOres.logger.info("Create is installed");
+//        } else BasicNetherOres.logger.info("Create is not installed");
         ClientHandler.registerBlockRender(event);
     }
 }
