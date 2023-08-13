dc11bd6363d596cdc08c313cb2090bd80ec631bd
cscot <cscot@nmd.so>
2023-08-13 19:33:51 -0400

    removed fabric multiloader logger messages

--- common/src/main/java/net/mcs3/basicnetherores/CommonClass.java
+++ common/src/main/java/net/mcs3/basicnetherores/CommonClass.java
@@ -19,18 +19,20 @@ public class CommonClass {
     // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
 
-        Constants.LOGGER.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
-        Constants.LOGGER.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
+//        Constants.LOGGER.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
+//        Constants.LOGGER.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
 
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
         // common code. A popular way to get around this is using Java's built-in service loader feature to create
         // your own abstraction layer. You can learn more about this in our provided services class. In this example
         // we have an interface in the common code and use a loader specific implementation to delegate our call to
         // the platform specific approach.
-        if (Services.PLATFORM.isModLoaded("bno")) {
 
-            Constants.LOGGER.info("Hello to Basic Nether Ores");
-        }
+
+//        if (Services.PLATFORM.isModLoaded("bno")) {
+//
+//            Constants.LOGGER.info("Hello to Basic Nether Ores");
+//        }
         BNOConfiguredFeatures.initialize();
         BNOPlacedFeatures.initialize();
 
